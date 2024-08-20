8b35a55bf44d07fc1113d5e297f4cf4840b1bd76
igi96 <nakanechny.ignat@gmail.com>
2024-08-20 18:15:39 +0200

    now it's working! (and I can close it)

--- main.py
+++ main.py
@@ -2,12 +2,30 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+pygame.init()
 from constants import *
 
-print("Starting asteroids!")
+
+def main():
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	print("Starting asteroids!")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				return
+		screen.fill("black")
+		pygame.display.flip()
+	
+	
 
 if __name__ == "__main__":
 	main()
 
-print(f"Screen width: {SCREEN_WIDTH}")
-print(f"Screen height: {SCREEN_HEIGHT}")
+
